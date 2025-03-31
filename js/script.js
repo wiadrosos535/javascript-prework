356c587ecda4269dccf92a14fb3146cf4b33cc0d
User <patryk.wiszniewski5@gmail.com>
2025-03-31 16:25:41 +0200

    computer and player move added

--- js/script.js
+++ js/script.js
@@ -8,7 +8,31 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let playerMove = 'papier';
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove);
+//Player move//
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+let playerMove = 'nieznany ruch';
+if(playerInput == 1){
+  playerMove = 'kamień';
+} else if(playerInput == 2){
+   playerMove = 'nożyce';
+} else if(playerInput == 3){
+   playerMove = 'papier';	
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+//computer move//
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = 'nieznany ruch';
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+   computerMove = 'nożyce';
+} else if(randomNumber == 3){
+   computerMove = 'papier';	
+}
+printMessage('Mój ruch to: ' + computerMove);
